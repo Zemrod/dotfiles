ea3393676ece7ec90427146b22aaddbab45adf53
Zemrod <6405202-Zemrod@users.noreply.gitlab.com>
2020-07-13 15:08:32 +0200

    removed unneded filter

--- .local/bin/bg.sh
+++ .local/bin/bg.sh
@@ -2,4 +2,4 @@
 
 # returns the path to the wallpaper on the screen you first set a Wallpaper (i.e Line 2 of the bg-saved.cfg)
 
-awk -F'=' '/file/ { if(NR==2) print $2 }' $HOME/.config/nitrogen/bg-saved.cfg
+awk -F'=' '{ if(NR==2) print $2 }' $HOME/.config/nitrogen/bg-saved.cfg
