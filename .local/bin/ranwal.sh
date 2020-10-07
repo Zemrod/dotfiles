e073cf48edcf6145f9ac52bdab3128c01cf99501
Zemrod <6405202-Zemrod@users.noreply.gitlab.com>
2020-10-07 15:30:01 +0200

    changed scaled to zoom-fill

--- .local/bin/ranwal.sh
+++ .local/bin/ranwal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-nitrogen --set-scaled --random --head=0 --save
-nitrogen --set-scaled --random --head=1 --save
+nitrogen --set-zoom-fill --random --head=0 --save
+nitrogen --set-zoom-fill --random --head=1 --save
 
 wal -n -i $(bg.sh)
 
