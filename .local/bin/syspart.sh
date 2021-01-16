0495fe089e6390a5e43ba56e98fb94cab341c887
Zemrod <sinon.adastrum@gmail.com>
2021-01-16 18:31:38 +0100

    switched out awk with own hawk-tool

--- .local/bin/syspart.sh
+++ .local/bin/syspart.sh
@@ -4,4 +4,7 @@
 
 disk=/dev/sda3
 
-printf " $(df -h -B M | grep ${disk} | awk '{print $3}')/$(df -h -B M | grep ${disk} | awk '{print $2}')"
+# printf " $(df -h -B M | grep ${disk} | awk '{print $3}')/$(df -h -B M | grep ${disk} | awk '{print $2}')"
+
+# same as above with own hawk tool
+printf " $(df -h -B M | grep ${disk} | hawk -f 3)/$(df -h -B M | grep ${disk} | hawk -f 2)"
