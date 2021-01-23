910645be4449acb28f0376ca6a85281c39280b7f
Zemrod <sinon.adastrum@gmail.com>
2021-01-23 09:04:43 +0100

    changed mail client

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -100,7 +100,7 @@ group_names = ["1: Dev",
 
 scratchpad = [ScratchPad("scratchpad", [
     # ScratchPad for sypheed
-    DropDown("mail", "sylpheed", height=0.7, opacity=1.0),
+    DropDown("mail", "trojita", height=0.7, opacity=1.0),
 
     # ScratchPad for liferea
     DropDown("rss", "liferea", height=0.7, opacity=1.0),
@@ -115,7 +115,7 @@ groups = scratchpad + groups
 
 keys.extend([
     Key([mod],          "s", lazy.group["scratchpad"].dropdown_toggle("mail"),
-        desc="Launch sylpheed"),
+        desc="Launch trojita"),
     Key([mod],          "f", lazy.group["scratchpad"].dropdown_toggle("rss"),
         desc="Launch liferea"),
     Key([mod, "shift"], "p", lazy.group["scratchpad"].dropdown_toggle("res"),
