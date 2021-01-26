c0fab82dfd208838ce76a0989d7c30220a5bf927
Zemrod <sinon.adastrum@gmail.com>
2021-01-26 09:48:14 +0100

    made script more dynamic

--- .local/bin/ip.sh
+++ .local/bin/ip.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 # name of the network device
-dev=enp2s0
+if ip a | grep inet | grep -q enp2s0;
+then
+	dev=enp2s0
+elif ip a | grep inet | grep -q wlan0;
+then
+	dev=wlan0
+else
+	dev=none
+fi
 
-printf " $(ip a | grep inet | grep $dev | rawk -f 2 | rawk -s '/' -f 1)"
+if $dev = "none";
+then
+	printf " no connection"
+else
+	printf " $(ip a | grep inet | grep $dev | rawk -f 2 | rawk -s '/' -f 1)"
+fi
