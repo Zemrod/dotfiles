876fcf9b5038d9a89f1b83389556bcfca15ef551
Zemrod <sinon.adastrum@gmail.com>
2021-02-16 17:10:46 +0100

    commented layout out

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -147,7 +147,7 @@ layouts = [
     # layout.VerticalTile(),
     # layout.Zoomy(),
     layout.Max(**layout_theme),
-    layout.Floating(**layout_theme),
+    # layout.Floating(**layout_theme),
 ]
 
 widget_defaults = dict(
