095192f29e974809d2d4a78c7423461359f36e54
Zemrod <sinon.adastrum@gmail.com>
2021-02-17 14:45:25 +0100

    rofi launch parameter change

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -11,7 +11,7 @@ import colors # symlink to $HOME/.cache/wal/colors.py
 mod = "mod4"
 terminal = "alacritty"
 altterminal = "st"
-rofi = "rofi -disable-history -case-sensitive -sort -show run"
+rofi = "rofi -disable-history -case-sensitive -sort -show run ssh window"
 
 keys = [
     # Switch between windows in current stack pane
