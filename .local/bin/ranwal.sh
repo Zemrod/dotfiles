b87e8546d37474b863acde3230706d61bc882df0
Zemrod <sinon.adastrum@gmail.com>
2021-06-26 11:23:18 +0200

    made a seperate script to generate the color scheme

--- .local/bin/ranwal.sh
+++ .local/bin/ranwal.sh
@@ -3,8 +3,4 @@
 nitrogen --set-zoom-fill --random --head=0 --save
 nitrogen --set-zoom-fill --random --head=1 --save
 
-wal -n -i $(bg.sh)
-
-# ST
-cd $HOME/.local/src/st
-sudo make clean install
+setcolor.sh
