65b1f5b008f4b37042b116523087166834bf61a8
Zemrod <sinon.adastrum@gmail.com>
2021-08-19 11:10:58 +0200

    root partition will now be picked by script

--- .local/bin/syspart.sh
+++ .local/bin/syspart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# prints the used space of the given disk
+# prints the used space of the root partition
 
-disk=/dev/sda3
+disk=$(df | grep "/dev/" | grep -v " /dev/" | grep -v " /[a-z]" | awk '{ print $1 }')
 
-printf " $(df -h -B M | grep $disk | awk '{print $3}')/$(df -h -B M | grep $disk | awk '{print $2}')"
+printf " $(df -h -B M | grep $disk | awk '{ print $3 }')/$(df -h -B M | grep $disk | awk '{ print $2 }')"
