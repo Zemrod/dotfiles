2190feceec63a12d074e648508a9821ea7e71f52
cinque <sinon.adastrum@gmail.com>
2022-07-11 08:32:59 +0200

    simple script to install vms with restrictions

---
+++ .local/bin/install_virt.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+virt-install --connect qemu:///system --name $1 --memory 4096 --vcpus=4 --cpu host --cdrom $2 --disk $HOME/.local/libvirt/images/$1.qcow2 --network default --video virtio --soundhw default --graphics spice --virt-type kvm --noautoconsole
