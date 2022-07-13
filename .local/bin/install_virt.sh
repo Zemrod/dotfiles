79c1c1b7ed9fbacd3e2936fab39266c3815a8bdb
cinque <sinon.adastrum@gmail.com>
2022-07-13 08:58:04 +0200

    attempt to detect the osname, else fallbakc to the linux2020 config

--- .local/bin/install_virt.sh
+++ .local/bin/install_virt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-virt-install --connect qemu:///system --osinfo linux2020 --name $1 --memory 4096 --vcpus=4 --cpu host --cdrom $2 --disk $HOME/.local/libvirt/images/$1.qcow2 --network default --video virtio --soundhw default --graphics spice --virt-type kvm --noautoconsole
+virt-install --connect qemu:///system --osinfo detect=on,name=linux2020 --name $1 --memory 4096 --vcpus=4 --cpu host --cdrom $2 --disk $HOME/.local/libvirt/images/$1.qcow2 --network default --video virtio --soundhw default --graphics spice --virt-type kvm --noautoconsole
