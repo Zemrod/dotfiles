2b9cf852c806aea56d62429eb73392ae4446936d
cinque <sinon.adastrum@gmail.com>
2022-11-05 09:57:36 +0100

    dbus-next issue fixed

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -140,18 +140,18 @@
     source-han-sans
   ];
 
- nixpkgs.overlays = [
-   (self: super: {
-      python3Packages = super.python3Packages.override {
-        overrides = pfinal: pprev: {
-          dbus-next = pprev.dbus-next.overridePythonAttrs (old: {
-            # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
-            checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
-          });
-        };
-      };
-    })
- ];
+# nixpkgs.overlays = [
+#   (self: super: {
+#      python3Packages = super.python3Packages.override {
+#        overrides = pfinal: pprev: {
+#          dbus-next = pprev.dbus-next.overridePythonAttrs (old: {
+#            # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
+#            checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
+#          });
+#        };
+#      };
+#    })
+# ];
   
   programs.zsh = {
     enable = true;
