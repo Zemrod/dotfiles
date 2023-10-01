893024dfb55f86e8ab8ff5d5d2c16f1530eeed60
cinque <sinon.adastrum@gmail.com>
2023-10-01 09:02:46 +0200

    temporary screen adjustment

--- .local/bin/screen.sh
+++ .local/bin/screen.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-primary=$(xrandr | grep connected | grep -v dis | grep primary | awk '{ print $1 }')
-secondary=$(xrandr | grep connected | grep -v dis | grep -v primary | awk '{ print $1 }')
+#primary=$(xrandr | grep connected | grep -v dis | grep primary | awk '{ print $1 }')
+primary=HDMI1
+#secondary=$(xrandr | grep connected | grep -v dis | grep -v primary | awk '{ print $1 }')
+secondary=DP3
 
 if [[ $1 = "on" ]]
 then
-	xrandr --output ${primary} --mode 1920x1200 --output ${secondary} --auto --right-of ${primary}
+	xrandr --output eDP1 --off --output ${primary} --auto --output ${secondary} --auto --right-of ${primary}
 
 	nitrogen --restore 2>/dev/null
 elif [[ $1 = "off" ]]
 then
-	xrandr --output ${secondary} --off
+	xrandr --output ${secondary} --off --output ${primary} --off --output eDP1 --mode 1920x1200
 fi
