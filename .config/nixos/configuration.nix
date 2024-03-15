08a78213768d3dbacf6b08597cc011df72bdffb9
cinque <sinon.adastrum@gmail.com>
2024-03-15 20:22:58 +0100

    wrong pinentry

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -221,7 +221,7 @@
 
   programs.gnupg.agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentryPackage = pkgs.pinentry-qt;
   };
   
   # List of services that you want to enable:
